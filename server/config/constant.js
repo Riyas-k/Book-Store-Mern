7a4a7ec2d27f0856d38ac31df8793f13e65b83f7
Riyazo4 <mohammedriyazriyaz04@gmail.com>
2023-11-20 02:45:24 +0530

    added .env

--- server/config/constant.js
+++ server/config/constant.js
@@ -2,4 +2,4 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = process.env.PORT;
-export const MONGO_URI ="mongodb+srv://mohammedriyazriyaz04:f8FAAsXpUmIhoAd3@cluster0.frihlon.mongodb.net/"
+export const MONGO_URI = process.env.MONGO_URI;
